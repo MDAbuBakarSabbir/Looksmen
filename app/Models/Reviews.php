300d6c65cdf194f2e1da5b48f173abf8544a2347
Sabbir Ahamed <sabbirahamed6242@gmail.com>
2026-06-29 22:15:38 +0600

    Add product relationship to Reviews model

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -9,7 +9,12 @@ class Reviews extends Model
     protected $guarded = [''];
 
     public function user()
-{
-    return $this->belongsTo(User::class, 'user_id');
-}
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
